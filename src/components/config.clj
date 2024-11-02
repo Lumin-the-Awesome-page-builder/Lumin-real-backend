1eed3f271804e1ce0ba5876f2c1afb1a6cfc8d26
dudosyka <a.hatson22@gmail.com>
2024-11-02 18:01:27 +0300

    build(app): uberjar based jar build

--- src/components/config.clj
+++ src/components/config.clj
@@ -1,6 +1,11 @@
 (ns components.config
   (:require [aero.core :refer [read-config]]))
 
+#_:clj-kondo/ignore
+(println (.getPath (clojure.java.io/resource "jwt.edn")))
+
 (defn fetch-config []
   #_:clj-kondo/ignore
-  (read-config (clojure.java.io/resource "config.edn")))
+  (read-config (clojure.java.io/resource "config.edn")
+               {:resolver {"jwt.edn" (clojure.java.io/resource "jwt.edn")
+                           "secrets.edn" (clojure.java.io/resource "secrets.edn")}}))
