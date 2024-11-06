806f124d81adbca8039103651267a61d8eb5362d
dudosyka <a.hatson22@gmail.com>
2024-11-06 16:33:52 +0300

    feat(app): project tree partial patching

--- src/modules/project/controller.clj
+++ src/modules/project/controller.clj
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [remove])
   (:require [compojure.core :refer [GET PATCH POST DELETE]]
             [ring.util.response :as response]
-            [modules.project.service :refer [get-by-id patch create remove]]))
+            [modules.project.service :refer [get-by-id patch create remove patch-tree]]))
 
 (defn prefixed [url] (str "/lumin/project" url))
 
@@ -17,6 +17,11 @@
            {:keys [id]} (:params request)
            {:keys [sub]} (:authorized request)]
        (response/response (patch datasource sub (parse-long id) (:params request)))))
+   (PATCH (prefixed "/:id/tree") request
+     (let [{:keys [datasource]} (:deps request)
+           {:keys [id]} (:params request)
+           {:keys [sub]} (:authorized request)]
+       (response/response (patch-tree datasource sub (parse-long id) (:params request)))))
    (DELETE (prefixed "/:id") request
      (let [{:keys [datasource]} (:deps request)
            {:keys [id]} (:params request)
