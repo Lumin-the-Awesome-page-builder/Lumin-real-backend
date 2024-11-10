70c1b8990f87abe1ba4e76597ff7df53bd00e5c1
h1l3xx <alksmlkv@gmail.com>
2024-11-10 21:14:50 +0300

    feat(library): router for uploading data about categories

--- src/modules/library/controller.clj
+++ src/modules/library/controller.clj
@@ -1,7 +1,7 @@
 (ns modules.library.controller
   (:require [compojure.core :refer [GET]]
             [ring.util.response :as response]
-            [modules.library.service :refer [get-all-projects get-all-widgets]]))
+            [modules.library.service :refer [get-all-projects get-all-widgets get-all-categories]]))
 
 (defn prefixed [url] (str "/lumin/user/library" url))
 
@@ -13,4 +13,9 @@
    (GET (prefixed "/projects") request
      (let [{:keys [datasource]} (:deps request)
            {:keys [sub]} (:authorized request)]
-       (response/response (get-all-projects datasource sub))))])
+       (response/response (get-all-projects datasource sub))))
+   (GET (prefixed "/categories") request
+     (let [{:keys [datasource]} (:deps request)
+           {:keys [sub]} (:authorized request)]
+       (response/response (get-all-categories datasource))))
+   ])
