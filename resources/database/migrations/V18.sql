700be5b0ff85ccdd0e17f481fc59e5d093d6c06e
dudosyka <a.hatson22@gmail.com>
2025-01-22 00:32:09 +0300

    feat(env): client env auto deploy

---
+++ resources/database/migrations/V18.sql
@@ -0,0 +1,7 @@
+insert into configuration (name, path, mapping, created_at)
+values (
+    'jvm-postgres',
+    '/home/docker-path/jvm-envs/jvm-postgres-env',
+    '{"bash":{"path":"jvm/app.jar","title":"JAR","description":"JAR file to run"},"postgres":{"path":"pg_init/init.sql","title":"SQL","description":"SQL file with database dump"}}',
+    1
+)
